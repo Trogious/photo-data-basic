ee69ceb3750f4fceca503345129869069c389781
Trog <trog@swmud.net>
2025-02-19 01:52:49 +0000

    n

--- test.mjs
+++ test.mjs
@@ -4,5 +4,5 @@ import inspect from './inspect.mjs'
 (async () => {
     // const data = await getPhotoData("https://some.site.net/test.jpg")
     const data = await getPhotoData("./test.jpg")
-    inspect("getPhotoData: ", data);
-})();
+    inspect("getPhotoData", data);
+})();;
