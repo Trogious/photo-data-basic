eaaf818baeea06a02cc0b0dfed33d687646a2a86
trog <trog@swmud.net>
2025-02-22 18:37:02 +0000

    n

--- s3_trigger.mjs
+++ s3_trigger.mjs
@@ -4,7 +4,7 @@ export const handler = async (event, context) => {
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     console.log("key: " + key);
     const extPart = key.slice(key.length - 5).toLowerCase();
-    if (extPart.endsWith(".jpg") && extPart.endsWith(".jpeg") && extPart.endsWith(".png")) {
+    if (extPart.endsWith(".jpg") || extPart.endsWith(".jpeg") || extPart.endsWith(".png")) {
         console.log("supported file: " + key);
     }
 }
